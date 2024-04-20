c9d44b63f040fd919363d6b6633934d3ad7381b0
Kaylee <kayleejolewis93@gmail.com>
2024-04-20 00:57:39 -0400

    A08

--- Exam1/PriorityQueueClojure/src/core.cljs
+++ Exam1/PriorityQueueClojure/src/core.cljs
@@ -9,58 +9,60 @@
 ;; https://www.programiz.com/dsa/priority-queue
 ;; https://www.geeksforgeeks.org/priority-queue-in-cpp-stl/
 
-;; function to insert an item into the priority queue
+;; Function to insert an item into the priority queue
 (defn insert [pq item pred]
   (let [insert-position (count (filter #(pred item %) pq))]
     (concat (take insert-position pq) (list item) (drop insert-position pq))))
 
-;; function to remove the item with the highest priority from the priority queue
+;; Function to remove the item with the highest priority from the priority queue
 (defn pop [pq]
   (if (empty? pq)
     (throw (js/Error. "Priority queue is empty"))
     (let [highest-priority-item (first pq)]
       (list highest-priority-item (rest pq)))))
 
-;; function to traverse the priority queue
+;; Function to traverse the priority queue
 (defn traverse [pq]
   (doseq [item pq]
     (println item)))
 
-;; main function
+;; Main function
 (defn main []
-  ;; create a priority queue
+  ;; Create a priority queue
   (let [priority-queue (atom [])
         priority-fn (atom nil)]
 
-    ;; function to push an item into the priority queue
+    ;; Function to push an item into the priority queue
     (defn push [item]
       (swap! priority-queue #(insert % item @priority-fn)))
 
-    ;; function to set the priority function
+    ;; Function to set the priority function
     (defn set-priority-fn [fn]
       (reset! priority-fn fn))
 
-    ;; function to test the priority queue
+    ;; Function to test the priority queue
     (defn test-priority-queue []
-      ;; set the priority function
+      ;; Set the priority function
       (set-priority-fn >)
-      ;; push items into the priority queue
+      ;; Push items into the priority queue
       (push 3)
       (push 1)
       (push 2)
-      ;; print the priority queue
+      ;; Print the priority queue
       (println "Priority queue:")
-      ;; traverse the priority queue
+      ;; Traverse the priority queue
       (traverse @priority-queue)
-      ;; print the item with the highest priority
+      ;; Print the item with the highest priority
       (println "Popped item with highest priority:")
 
-      ;; pop the item with the highest priority
+      ;; Pop the item with the highest priority
       (let [[item _] (pop @priority-queue)]
         (println item))))
 
-  ;; test the priority queue
+  ;; Test the priority queue
   (test-priority-queue))
 
-;; call the main function
+;; Call the main function
 (main)
+
+;; 
